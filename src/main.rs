3ffd7113f1ee3267a38292b95206f5fc336fccad
Timo Netzer <exodiquas@gmail.com>
2021-02-18 19:58:01 +0100

    yaml config file for the cli, cleaned up a bit in the cli

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,15 @@
 use std::time::Instant;
 
-use clap::{App, Arg};
+use clap::{load_yaml, App};
 use obbdimal::bdd::bdd_manager::BddManager;
 use obbdimal::input::parser::ParserSettings;
 use obbdimal::{bdd::bdd_ds::InputFormat, input::static_ordering::StaticOrdering};
 
 fn main() {
-    let matches = App::new("OBDDimal")
-        .version("0.1")
-        .author("Timo Netzer <timo.netzer@uni-ulm.de>")
-        .about("A experimental, parallelized BDD library written in Rust.")
-        .arg(
-            Arg::new("INPUT")
-                .short('i')
-                .long("input")
-                .value_name("FILE")
-                .about("Path to the input file (currently only dimacs cnf files are supported)")
-                .takes_value(true)
-                .required_unless_present("LOAD"),
-        )
-        .arg(
-            Arg::new("LOAD")
-                .short('l')
-                .long("load")
-                .value_name("FILE")
-                .about("Path to the input file of a previously saved BDD")
-                .takes_value(true)
-                .required_unless_present("INPUT"),
-        )
-        .arg(
-            Arg::new("STATIC VARIABLE ORDERING")
-                .short('s')
-                .long("static")
-                .about("Static variable heuristic which is applied before the BDD is generated. Currently supported: NONE, FORCE")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::new("OUTPUT")
-                .short('o')
-                .long("output")
-                .value_name("FILE")
-                .takes_value(true)
-                .about("Path where the serialized BDD should be saved"),
-        )
-        .arg(
-            Arg::new("TIMER")
-                .short('t')
-                .long("timer")
-                .about("Also prints how long the program was running (Not very exact for short time periods)"),
-        )
-        .arg(
-            Arg::new("VERBOSE")
-                .short('v')
-                .long("verbose")
-                .about("Prints the selected options before running the calculations")
-        )
-        .get_matches();
+    let yaml = load_yaml!("clap_config.yaml");
+    let matches = App::from(yaml).get_matches();
 
-    match matches.value_of("LOAD") {
+    match matches.value_of("load") {
         Some(i) => {
             let data = std::fs::read_to_string(i).unwrap();
             let mgr = BddManager::new();
@@ -68,7 +20,7 @@ fn main() {
         None => {}
     }
 
-    let path = match matches.value_of("INPUT") {
+    let path = match matches.value_of("input") {
         Some(i) => i,
         None => {
             println!("No input file specified.");
@@ -78,7 +30,7 @@ fn main() {
 
     let mut selected_output_path = "NONE";
 
-    let output_path = match matches.value_of("OUTPUT") {
+    let output_path = match matches.value_of("output") {
         Some(i) => {
             selected_output_path = i;
             selected_output_path
@@ -92,7 +44,7 @@ fn main() {
 
     let mut selected_static_ordering = "NONE";
 
-    let static_ordering = match matches.value_of("STATIC VARIABLE ORDERING") {
+    let static_ordering = match matches.value_of("preorder") {
         Some("FORCE") => {
             selected_static_ordering = "FORCE";
             StaticOrdering::FORCE
@@ -100,7 +52,7 @@ fn main() {
         _ => StaticOrdering::NONE,
     };
 
-    if matches.is_present("VERBOSE") {
+    if matches.is_present("verbose") {
         println!("Selected input path: {}\nSelected output path: {}\nSelected static variable ordering: {}\nSelected timer state: {}\n", path, selected_output_path, selected_static_ordering, matches.is_present("TIMER"));
     }
 
@@ -119,7 +71,7 @@ fn main() {
     match sat_count {
         Ok(num) => {
             println!("Number of solutions for the BDD: {:?}", num);
-            if matches.is_present("TIMER") {
+            if matches.is_present("timer") {
                 println!("It took {:?} to complete.", timer.elapsed());
             }
         }
