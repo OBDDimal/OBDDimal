6faf7b916ed22291d3fcb65bc31e18ce9281265e
h3ssto <tobias.hess@uni-ulm.de>
2022-06-09 22:21:57 +0200

    Pushed proof of concept

---
+++ src/core/bdd_manager.rs
@@ -0,0 +1,464 @@
+use crate::Instance;
+use crate::core::bdd_node::DDNode;
+
+use rustc_hash::{FxHashMap, FxHashSet};
+
+use num_bigint::{BigUint};
+use num_traits::{Zero, One};
+
+use rand::Rng;
+
+pub struct DDManager{
+    pub nodes: FxHashMap<u32, DDNode>,
+    order: Vec<u32>,
+    var2nodes : Vec<FxHashSet<DDNode>>,
+    c_table: FxHashMap<(u32, u32, u32), u32>
+}
+
+impl Default for DDManager{
+
+    fn default() -> Self {
+        let mut man = DDManager {
+            nodes: FxHashMap::default(),
+            order: Vec::new(),
+            var2nodes: Vec::new(),
+            c_table: FxHashMap::default()
+        };
+
+        man.bootstrap();
+        man
+    }
+}
+
+pub fn align_clauses(clauses:&Vec<Vec<i32>>, order:&Vec<u32>) -> Vec<usize> {
+
+    let mut shuffle:Vec<(usize, f32)> = Vec::default();
+
+    for (i,clause) in clauses.iter().enumerate(){
+
+        for x in clause{
+            let y = x.abs();
+        }
+
+        let min = clause.iter().map(|x| x.abs()).min().unwrap();
+        let max = clause.iter().map(|x| x.abs()).max().unwrap();
+
+        shuffle.push((i, (clause.len() as f32 *(max-min) as f32)));
+    }
+
+    shuffle.sort_by(|x,y| x.1.partial_cmp(&y.1).unwrap());
+    shuffle.iter().map(|(x,_)| *x).collect::<Vec<usize>>()
+}
+
+impl DDManager{
+
+    pub fn from_instance(&mut self, instance:&mut Instance, order:Option<Vec<u32>>) -> u32 {
+
+
+        if order.is_some() {
+            self.order = order.unwrap();
+            instance.clause_order = Some(align_clauses(&instance.clauses, &self.order));
+        }
+
+        let mut bdd = self.one();
+
+        let mut index_giver = 
+            (0..instance.clauses.len()).collect::<Vec<usize>>();
+
+        if instance.clause_order.is_some() {
+            index_giver = align_clauses(&instance.clauses, &self.order)
+        }
+
+        let iter = index_giver.iter_mut();
+
+        let mut n = 1;
+        for i in iter {
+            let clause = &instance.clauses[*i];
+
+            println!("{:?}", clause);
+
+            let mut cbdd = self.zero();
+            for x in clause {
+
+                let node = if *x < (0 as i32) {
+                    self.nith_var(-x as u32)
+                }else{
+                    self.ith_var(*x as u32)
+                };
+
+                cbdd = self.or(node, cbdd);
+            }
+
+            bdd = self.and(cbdd, bdd);
+
+            self.purge_retain(bdd);
+
+            println!("{:?} ({:?}/{:?})", &self.nodes.len(), n, &instance.clauses.len());
+            n += 1;
+        }
+
+        bdd
+    }
+
+    fn bootstrap(&mut self) -> () {
+        
+        let zero = Box::new(DDNode {id:0, var:0, low:0, high: 0, misc: 0});
+        let one = Box::new(DDNode {id:1, var:0, low:1, high: 1, misc: 0});
+
+        self.add_node(zero);
+        self.add_node(one);
+    }
+
+    fn ensure_order(&mut self, target:usize) {
+
+        let old_size = self.order.len();
+
+        if target < old_size {
+            return;    
+        }
+        
+        self.order.resize(target + 1, 0);
+        let mut y = old_size;
+
+        for x in old_size..self.order.len() {
+            self.order[x] = y as u32;
+            y = y + 1;
+        }
+
+        self.order[0] = y as u32;
+
+        println!("RESIZE: {:?}", self.order);
+    }
+
+    fn add_node(&mut self, mut node:Box<DDNode>) -> u32 {
+
+        if node.id == 0 && node.var != 0{
+
+            let mut id = rand::thread_rng().gen::<u32>();
+
+            while self.nodes.get(&id).is_some(){
+                id = rand::thread_rng().gen::<u32>();
+            }
+
+            node.id = id;
+        }
+
+        let id = node.id;
+        let var = node.var;
+
+        let node2 = node.clone();
+
+        self.nodes.insert(id, *node);
+
+        while self.var2nodes.len() <= (var as usize){
+            self.var2nodes.push(FxHashSet::default())
+        }
+
+        self.ensure_order(var as usize);
+
+        self.var2nodes[var as usize].insert(*node2);
+
+        id
+    }
+
+    fn node_get_or_create(&mut self, node:Box<DDNode>) -> u32  {
+
+        if self.var2nodes.len() <= (node.var as usize) {
+            return self.add_node(node);
+        }
+
+        let res = self.var2nodes[node.var as usize].get(&*node);
+
+        match res {
+            Some(stuff) => stuff.id,
+            None => self.add_node(node)
+        }
+    }
+
+    fn is_sat(&self, node:u32) -> bool {
+        node != 0
+    }
+
+    //------------------------------------------------------------------------//
+    // Constants
+
+    fn zero(&self) -> u32 {
+        0
+    }
+
+    fn one(&self) -> u32 {
+        1        
+    }
+
+    //------------------------------------------------------------------------//
+    // Variables
+
+    pub fn ith_var(&mut self, var:u32) -> u32 {
+        let v = Box::new(DDNode {id: 0, var: var, low:0, high:1, misc:0});
+
+        if self.var2nodes.len() > (var as usize){
+
+            let x = self.var2nodes[var as usize].get(&*v);
+
+            if x.is_some(){
+                return x.unwrap().id
+            }
+        }
+
+        self.add_node(v)
+    }
+
+    pub fn nith_var(&mut self, var:u32) -> u32 {
+        let v = Box::new(DDNode {id: 0, var: var, low:1, high:0, misc:0});
+
+        if self.var2nodes.len() > (var as usize){
+
+            let x = self.var2nodes[var as usize].get(&*v);
+
+            if x.is_some(){
+                return x.unwrap().id
+            }
+        }
+
+        self.add_node(v)
+    }
+
+    //------------------------------------------------------------------------//
+    // Unitary Operations
+
+    fn not(&mut self, f:u32) -> u32 {
+        self.ite(f, 0, 1)
+    }
+
+    //------------------------------------------------------------------------//
+    // Binary Operations
+
+    pub fn and(&mut self, f:u32, g:u32) -> u32 {
+        self.ite(f, g, 0)
+    }
+
+    pub fn or(&mut self, f:u32, g:u32) -> u32 {
+        self.ite(f, 1, g)
+    }
+
+    fn xor(&mut self, f:u32, g:u32) -> u32 {
+
+        let ng = self.not(g);
+
+        self.ite(f, ng, g)
+    }
+
+    //------------------------------------------------------------------------//
+    // N-ary Operations
+
+    fn min_by_order(&self, fvar:u32, gvar:u32, hvar:u32) -> u32{
+
+        let list:Vec<u32> = vec![fvar, gvar, hvar].into_iter().filter(|x| *x > 0).collect::<Vec<u32>>();
+
+        // println!("{:?}", list);
+        let tlist = list.iter().map(|&x| self.order[x as usize]).collect::<Vec<u32>>();
+        // println!("{:?}", list);
+
+        let min:u32 = *tlist.iter().min().unwrap();
+        let index = tlist.iter().position(|&x| x == min).unwrap();
+
+        list[index] 
+    }
+
+    fn ite(&mut self, f:u32, g:u32, h:u32) -> u32 {
+        
+        match (f, g, h){
+            (_, 1, 0) => f,
+            (_, 0, 1) => self.not(f),
+            (1, _, _) => g,
+            (0, _, _) => h,
+            (_, t, e) if t == e => g,
+            (_, _, _) => {
+
+                let cache = self.c_table.get(&(f,g,h));
+
+                if cache.is_some(){
+                    return *cache.unwrap();
+                }
+
+                let fnode = &self.nodes.get(&f).unwrap();
+                let gnode = &self.nodes.get(&g).unwrap();
+                let hnode = &self.nodes.get(&h).unwrap();
+
+                let top:u32 = self.min_by_order(fnode.var, gnode.var, hnode.var);
+
+                let fxt = fnode.restrict(top, &self.order, true);
+                let gxt = gnode.restrict(top, &self.order, true);
+                let hxt = hnode.restrict(top, &self.order, true);
+
+                let fxf = fnode.restrict(top, &self.order, false);
+                let gxf = gnode.restrict(top, &self.order, false);
+                let hxf = hnode.restrict(top, &self.order, false);
+                
+                let high = self.ite(fxt, gxt, hxt);
+                let low = self.ite(fxf, gxf, hxf);
+                
+                if low == high {
+                    return low;
+                }
+
+                let node = Box::new(DDNode {id: 0, var: top, low: low, high: high, misc: 0});
+
+                let out = self.node_get_or_create(node);
+
+                self.c_table.insert((f,g,h), out);
+
+                out
+            }
+        }
+    }
+
+    //------------------------------------------------------------------------//
+    // Builders
+
+    /// Creates an XOR "ladder" 
+    /// 
+    /// 
+    fn xor_prim(&mut self, vars:Vec<u32>) -> u32 {
+       0 
+    }
+
+
+    fn verify(&self, f:u32,  trues: Vec<u32>) -> bool {
+        let mut values:Vec<bool> = vec![false; self.var2nodes.len() + 1];
+
+        for x in trues{
+            let x:usize = x as usize;
+
+            if x < values.len() {
+                values[x] = true;
+            }else{
+                values[x] = false;
+            }
+        }
+
+        let mut node_id = f;
+
+        while node_id >= 2 {
+            let node = &self.nodes.get(&node_id).unwrap();
+
+            if values[node.var as usize] {
+                node_id = node.high;
+            }else{
+                node_id = node.low;
+            }
+        }
+        
+        return node_id == 1
+    }
+
+    pub fn sat_count(&self, f:u32) -> BigUint {
+        self.sat_count_rec(f, &mut FxHashMap::default())
+    }
+
+    fn sat_count_rec(&self, f:u32, cache:&mut FxHashMap<u32, BigUint>) -> BigUint {
+        let mut total:BigUint = Zero::zero();
+        let node_id = f;
+
+        if node_id == 0 {
+            return Zero::zero();
+        }else if node_id == 1{
+            return One::one();
+        }else{
+            let node = &self.nodes.get(&node_id).unwrap();
+
+            let low = &self.nodes.get(&node.low).unwrap();
+            let high = &self.nodes.get(&node.high).unwrap();
+
+            let low_jump = if low.var == 0 {
+                self.order.len() as u32 - self.order[node.var as usize] - 1
+            }else{
+                self.order[low.var as usize] - self.order[node.var as usize] - 1
+            };
+
+            let high_jump = if high.var == 0 {
+                self.order.len() as u32 - self.order[node.var as usize] - 1
+            }else{
+                self.order[high.var as usize] - self.order[node.var as usize] - 1
+            };
+
+            let low_fac = BigUint::parse_bytes(b"2", 10).unwrap().pow(low_jump);
+            let high_fac = BigUint::parse_bytes(b"2", 10).unwrap().pow(high_jump);
+
+            total += match cache.get(&node.low) {
+                Some(x) => x*low_fac,
+                None => self.sat_count_rec(node.low, cache)*low_fac
+            };
+
+            total += match cache.get(&node.high) {
+                Some(x) => x*high_fac,
+                None => self.sat_count_rec(node.high, cache)*high_fac
+            };            
+        };
+
+        cache.insert(f, total.clone());
+
+        return total
+    }
+
+    pub fn count_active(&self, f:u32) -> u32{
+
+        let mut nodes:FxHashSet<u32> = FxHashSet::default();
+
+        let mut stack:Vec<u32> = Vec::new();
+
+        stack.push(f);
+
+        while !stack.is_empty() {
+
+            let x = stack.pop().unwrap();
+
+            if nodes.contains(&x) {
+                continue
+            }
+
+            let node = self.nodes.get(&x).unwrap();
+
+            stack.push(node.low);
+            stack.push(node.high);
+            nodes.insert(x);
+        }
+
+
+        nodes.len() as u32
+    }
+
+    pub fn purge_retain(&mut self, f:u32) {
+        let mut keep:FxHashSet<u32> = FxHashSet::default();
+
+        let mut stack:Vec<u32> = Vec::new();
+
+        stack.push(f);
+
+        while !stack.is_empty() {
+
+            let x = stack.pop().unwrap();
+
+            if keep.contains(&x) {
+                continue;
+            }
+
+            let node = self.nodes.get(&x).unwrap();
+
+            stack.push(node.low);
+            stack.push(node.high);
+            keep.insert(x);
+        }
+
+        let mut garbage = self.nodes.clone();
+
+        garbage.retain(|&x, _| !keep.contains(&x) && x > 1);
+
+        for x in &garbage {
+            self.var2nodes[x.1.var as usize].remove(x.1);
+            self.nodes.remove(x.0);
+        }
+
+        self.c_table.retain(|_, x| keep.contains(x));
+    }
+}
---
+++ src/core/bdd_node.rs
@@ -0,0 +1,54 @@
+use std::hash::{Hash, Hasher};
+
+#[derive(Debug, Copy, Clone)]
+pub struct DDNode {
+    pub id:     u32,
+    pub var:    u32,
+    pub low:    u32,
+    pub high:   u32,
+    pub misc:   u32
+}
+
+impl PartialEq for DDNode {
+    fn eq(&self, that:&Self) -> bool {
+            self.var == that.var
+            && self.low == that.low
+            && self.high == that.high
+    }
+}
+
+impl Eq for DDNode {}
+
+impl DDNode {
+
+    pub fn restrict(&self, top:u32, order:&Vec<u32>, val:bool) -> u32 {
+
+        if self.var == 0 {
+            return self.id
+        }
+
+        if order[top as usize] < order[self.var as usize] {
+            return self.id;
+        }
+
+        if top == self.var {
+            if val {
+                return self.high;
+            }else{
+                return self.low;
+            }
+        }
+
+        panic!("Should not be possible");
+    }   
+}
+
+impl Hash for DDNode {
+
+    fn hash<H: Hasher>(&self, state: &mut H){
+        self.var.hash(state);
+        self.low.hash(state);
+        self.high.hash(state);
+    }
+
+}
