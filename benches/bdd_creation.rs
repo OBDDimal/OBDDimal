9bf33ef5f053388191f3b77a0739dc1c6ff142a4
Jonas Otto <jonas@jonasotto.com>
2022-06-22 12:44:32 +0200

    fix sandwich.dimacs bdd creation benchmark name

--- benches/bdd_creation.rs
+++ benches/bdd_creation.rs
@@ -2,12 +2,12 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 use obddimal::{bdd_manager::DDManager, dimacs};
 
-pub fn sandwich_ssat_benchmark(c: &mut Criterion) {
+pub fn sandwich_create_benchmark(c: &mut Criterion) {
     let cnf = dimacs::parse_dimacs("examples/sandwich.dimacs");
-    c.bench_function("ssat sandwich bench", |b| {
+    c.bench_function("sandwich.dimacs bdd creation", |b| {
         b.iter(|| DDManager::from_instance(&mut cnf.clone(), None))
     });
 }
 
-criterion_group!(benches, sandwich_ssat_benchmark);
+criterion_group!(benches, sandwich_create_benchmark);
 criterion_main!(benches);
