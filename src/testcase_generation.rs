9e22b7e4914436a0f88d5ad7c0ef1ab67105b322
Jonas Otto <jonas@jonasotto.com>
2022-09-02 11:49:25 +0200

    add randomly generated testcase

---
+++ src/testcase_generation.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+use bitvec::prelude::*;
+
+use rand::Rng;
+
+/// This prints the one-columns of a random truth table with 8 variables,
+/// for testcase generation. The result is to be used in src/bdd_manager/test.rs.
+fn main() {
+    let mut truthtable: HashMap<u8, bool> = Default::default();
+
+    let mut rng = rand::thread_rng();
+
+    for i in 0u8..=255u8 {
+        let f: bool = rng.gen();
+        truthtable.insert(i, f);
+    }
+
+    for (k, v) in truthtable {
+        if v {
+            println!("{},", k.view_bits::<Lsb0>())
+        }
+    }
+}
