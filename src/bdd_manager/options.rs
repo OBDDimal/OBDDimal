4326d2d781adca8b6298008831c4d38cf5bd58c9
Jonas Otto <jonas@jonasotto.com>
2022-09-05 20:06:32 +0200

    cleanup

--- src/bdd_manager/options.rs
+++ src/bdd_manager/options.rs
@@ -5,13 +5,11 @@ pub struct Options {
 }
 
 impl Options {
-    #[allow(unused)]
     pub fn with_progressbars(mut self) -> Options {
         self.progressbars = true;
         self
     }
 
-    #[allow(unused)]
     pub fn with_dvo(mut self) -> Options {
         self.enable_dvo = true;
         self
