f4de4a42ccd52fd010f2e8f3010f2c8b653db38b
Daniel Klier <daniel.klier@uni-ulm.de>
2024-02-12 14:46:59 +0100

    Add basic structure for BDD export/import

---
+++ src/store.rs
@@ -0,0 +1,2 @@
+pub mod bdd;
+pub mod dddmp;
---
+++ src/store/bdd.rs
@@ -0,0 +1,67 @@
+use crate::{core::bdd_manager::DDManager, core::bdd_node::NodeID};
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+#[derive(Serialize, Deserialize)]
+struct BddFile {
+    statistics: Statistics,
+    bdd: Bdd,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Statistics {
+    void: bool,
+    count: usize,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Bdd {
+    order: Vec<usize>,
+    roots: Vec<NodeID>,
+    // node format:
+    // id_node id_var id_high id_low
+    nodes: String,
+}
+
+impl DDManager {
+    /// Reads a (multi-rooted) BDD from a .bdd file.
+    ///
+    /// * `filename` - Name of the .bdd file.
+    ///
+    /// ```
+    /// # use obddimal::core::bdd_manager::DDManager;
+    /// let (man, bdds) = DDManager::from_bdd_file("testbdd.bdd").unwrap();
+    /// ```
+    pub fn from_bdd_file(filename: String) -> (DDManager, Vec<NodeID>) {
+        let bdd_file: BddFile = toml::from_str(
+            &fs::read_to_string(filename).expect("Failed to read bdd file: Could not read file!"),
+        )
+        .expect("Failed to read bdd file: Could not parse toml!");
+        todo!();
+    }
+
+    /// Writes a (multi-rooted) BDD to a .bdd file.
+    ///
+    /// * `filename` - Name of the .bdd file.
+    /// * `roots` - The roots of the BDD.
+    ///
+    pub fn to_bdd_file(filename: String, roots: Vec<NodeID>) -> Result<(), String> {
+        let void = true; // TODO
+        let count = 42; // TODO
+        let order = Vec::new(); // TODO
+        let nodes = "".to_string(); // TODO
+        let bdd_file = BddFile {
+            statistics: Statistics { void, count },
+            bdd: Bdd {
+                order,
+                roots,
+                nodes,
+            },
+        };
+        fs::write(
+            filename,
+            toml::to_string_pretty(&bdd_file).map_err(|e| e.to_string())?,
+        )
+        .map_err(|e| e.to_string())
+    }
+}
