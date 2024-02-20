a51e56180eefd142617f1d6fe353bac3ef7c89d3
Daniel Klier <daniel.klier@uni-ulm.de>
2024-02-20 16:38:02 +0100

    Add parsing of .bdd file.

--- src/store/bdd.rs
+++ src/store/bdd.rs
@@ -2,7 +2,13 @@ use std::fs;
 
 use serde::{Deserialize, Serialize};
 
-use crate::core::{bdd_manager::DDManager, bdd_node::NodeID};
+use crate::{
+    core::{
+        bdd_manager::DDManager,
+        bdd_node::{NodeID, VarID},
+    },
+    misc::hash_select::HashMap,
+};
 
 #[derive(Serialize, Deserialize)]
 struct BddFile {
@@ -18,7 +24,7 @@ struct Statistics {
 
 #[derive(Serialize, Deserialize)]
 struct Bdd {
-    order: Vec<usize>,
+    order: Vec<VarID>,
     roots: Vec<NodeID>,
     // node format:
     // id_node id_var id_high id_low
@@ -35,10 +41,48 @@ impl DDManager {
     /// //let (man, bdds) = DDManager::load_from_bdd_file("testbdd.bdd".to_string()).unwrap();
     /// ```
     pub fn load_from_bdd_file(filename: String) -> Result<(DDManager, Vec<NodeID>), String> {
-        let _bdd_file: BddFile =
+        let bdd_file: BddFile =
             toml::from_str(&fs::read_to_string(filename).map_err(|e| e.to_string())?)
                 .map_err(|e| e.to_string())?;
-        todo!();
+        let varorder: Vec<VarID> = bdd_file.bdd.order;
+        let roots: Vec<NodeID> = bdd_file.bdd.roots;
+
+        let mut terminals: (Option<NodeID>, Option<NodeID>) = (None, None);
+        let nodes = bdd_file
+            .bdd
+            .nodes
+            .lines()
+            .filter(|line| !line.trim().is_empty())
+            .map(|line| {
+                let line: Vec<_> = line.split_whitespace().map(str::to_string).collect();
+                if line.len() != 4 {
+                    Err("Node list contains unexpected line!".to_string())
+                } else {
+                    let id = NodeID(line[0].parse::<usize>().map_err(|e| e.to_string())?);
+                    let varid = VarID(line[1].parse::<usize>().map_err(|e| e.to_string())?);
+                    let high = NodeID(line[2].parse::<usize>().map_err(|e| e.to_string())?);
+                    let low = NodeID(line[3].parse::<usize>().map_err(|e| e.to_string())?);
+                    if high == low {
+                        if high == NodeID(1) {
+                            terminals.0 = Some(id);
+                        } else if high == NodeID(0) {
+                            terminals.1 = Some(id);
+                        } else {
+                            return Err("Invalid terminal node!".to_string());
+                        }
+                    }
+                    Ok((id, (varid, high, low)))
+                }
+            })
+            .try_collect::<HashMap<NodeID, (VarID, NodeID, NodeID)>>()?;
+
+        let terminals = match terminals {
+            (Some(t_high), Some(t_low)) => Ok((t_high, t_low)),
+            _ => Err("Terminal nodes missing!".to_string()),
+        }?;
+
+        let mut bdd = DDManager::default();
+        Ok(bdd.load_bdd_from_nodetable(&nodes, &varorder, &roots, &terminals))
     }
 
     /// Writes a (multi-rooted) BDD to a .bdd file.
