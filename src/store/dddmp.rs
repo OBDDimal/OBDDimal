d08621bad69ee7f574dd6cbd61353400a1405c09
Daniel Klier <daniel.klier@uni-ulm.de>
2024-03-04 16:16:52 +0100

    Fixed docs.

--- src/store/dddmp.rs
+++ src/store/dddmp.rs
@@ -42,7 +42,8 @@ impl DDManager {
     ///
     /// ```
     /// # use obddimal::core::bdd_manager::DDManager;
-    /// let Ok((man, bdds)) = DDManager::load_from_dddmp_file("sandwich.dimacs.dddmp".to_string());
+    /// let (man, bdds) =
+    /// DDManager::load_from_dddmp_file("sandwich.dimacs.dddmp".to_string()).unwrap();
     /// ```
     pub fn load_from_dddmp_file(filename: String) -> Result<(DDManager, Vec<NodeID>), String> {
         let bcdd =
