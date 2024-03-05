2994f0b81f6267cb26955fc6f259c9a8d17da44a
Daniel-Klier <Daniel-Klier@users.noreply.github.com>
2024-03-05 14:43:01 +0000

    deploy: 655885df7fefc8a83d5bd0bd938ad0ac79eebd0f

--- rustdoc/src-files.js
+++ rustdoc/src-files.js
@@ -1,5 +1,5 @@
-var srcIndex = JSON.parse('{\
-"obddimal":["",[["build",[],["from_dimacs.rs"]],["core",[["dvo",[],["dvo_schedules.rs"]]],["bdd_manager.rs","bdd_node.rs","dvo.rs","options.rs","order.rs","reduce.rs","swap.rs","test.rs"]],["misc",[],["hash_select.rs","if_some.rs","static_ordering.rs"]],["util",[],["graphviz.rs","sat.rs"]]],["build.rs","core.rs","lib.rs","misc.rs","util.rs"]],\
-"testcase_generation":["",[],["testcase_generation.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["obddimal",["",[["build",[],["from_dimacs.rs"]],["core",[["dvo",[],["dvo_schedules.rs"]]],["bdd_manager.rs","bdd_node.rs","dvo.rs","options.rs","order.rs","reduce.rs","swap.rs","test.rs"]],["misc",[],["hash_select.rs","if_some.rs","static_ordering.rs"]],["util",[],["graphviz.rs","sat.rs"]]],["build.rs","core.rs","lib.rs","misc.rs","util.rs"]]],\
+["testcase_generation",["",[],["testcase_generation.rs"]]]\
+]'));
 createSrcSidebar();
