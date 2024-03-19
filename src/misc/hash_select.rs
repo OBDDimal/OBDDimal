5a32700198ebc99180bd9cca862fb210ed3bed02
Daniel Klier <daniel.klier@uni-ulm.de>
2024-03-19 12:50:07 +0100

    Fix typo.

--- src/misc/hash_select.rs
+++ src/misc/hash_select.rs
@@ -1,4 +1,4 @@
-//! Type aliases for HashMap and HashSet to be used throuout the codebase
+//! Type aliases for HashMap and HashSet to be used throughout the codebase
 
 //pub use fnv::{FnvHashMap as HashMap, FnvHashSet as HashSet};
 pub use rustc_hash::{FxHashMap as HashMap, FxHashSet as HashSet};
