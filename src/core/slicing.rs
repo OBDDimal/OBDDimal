c4faf8329f41155bcd4058ec3a28aa7ca017c6c4
Daniel Klier <daniel.klier@uni-ulm.de>
2024-03-20 16:39:07 +0100

    Added slicing approach based on exists.

--- src/core/slicing.rs
+++ src/core/slicing.rs
@@ -193,6 +193,101 @@ impl DDManager {
 
         reachable
     }
+
+    /// Creates a slice of a BDD containing only the given variables.
+    ///
+    /// * `keep` - The variables to keep
+    /// * `root` - The root node of the BDD that is supposed to be sliced
+    ///
+    /// # Returns
+    /// The id of the root node of the slice.
+    ///
+    /// # Notes
+    /// May create temporary nodes, consider using [`purge_retain_multi`] or [`purge_retain`]
+    /// afterwards.
+    ///
+    /// [`purge_retain_multi`]: crate::core::bdd_manager::DDManager::purge_retain_multi
+    /// [`purge_retain`]: crate::core::bdd_manager::DDManager::purge_retain
+    #[inline]
+    pub fn create_slice_exists(&mut self, root: NodeID, keep: &HashSet<VarID>) -> NodeID {
+        *self.create_slices_exists(&[root], keep).get(&root).unwrap()
+    }
+
+    /// Creates a slice of a BDD containing all except the given variables.
+    ///
+    /// * `remove` - The variables to remove
+    /// * `root` - The root node of the BDD that is supposed to be sliced
+    ///
+    /// # Returns
+    /// The id of the root node of the slice.
+    ///
+    /// # Notes
+    /// May create temporary nodes, consider using [`purge_retain_multi`] or [`purge_retain`]
+    /// afterwards.
+    ///
+    /// [`purge_retain_multi`]: crate::core::bdd_manager::DDManager::purge_retain_multi
+    /// [`purge_retain`]: crate::core::bdd_manager::DDManager::purge_retain
+    #[inline]
+    pub fn create_slice_without_vars_exists(
+        &mut self,
+        root: NodeID,
+        remove: &HashSet<VarID>,
+    ) -> NodeID {
+        self.exists(root, remove)
+    }
+
+    /// Creates slices of BDDs containing only the given variables.
+    ///
+    /// * `keep` - The variables to keep
+    /// * `roots` - The root nodes of the BDDs that are supposed to be sliced
+    ///
+    /// # Returns
+    /// A HashMap giving the id of the root node of the created slice for each BDD (the keys to the
+    /// HashMap are the ids of the root nodes of the original BDDs, as given in the node_ids
+    /// parameter).
+    ///
+    /// # Notes
+    /// May create temporary nodes, consider using [`purge_retain_multi`] or [`purge_retain`]
+    /// afterwards.
+    ///
+    /// [`purge_retain_multi`]: crate::core::bdd_manager::DDManager::purge_retain_multi
+    /// [`purge_retain`]: crate::core::bdd_manager::DDManager::purge_retain
+    pub fn create_slices_exists(
+        &mut self,
+        roots: &[NodeID],
+        keep: &HashSet<VarID>,
+    ) -> HashMap<NodeID, NodeID> {
+        let remove = (1..(self.var2level.len() - 1))
+            .map(VarID)
+            .filter(|var_id| !keep.contains(var_id))
+            .collect::<HashSet<_>>();
+
+        self.create_slices_without_vars_exists(roots, &remove)
+    }
+
+    /// Creates slices of BDDs containing all except the given variables.
+    ///
+    /// * `remove` - The variables to remove
+    /// * `roots` - The root nodes of the BDDs that are supposed to be sliced
+    ///
+    /// # Returns
+    /// A HashMap giving the id of the root node of the created slice for each BDD (the keys to the
+    /// HashMap are the ids of the root nodes of the original BDDs, as given in the node_ids
+    /// parameter).
+    ///
+    /// # Notes
+    /// May create temporary nodes, consider using [`purge_retain_multi`] or [`purge_retain`]
+    /// afterwards.
+    ///
+    /// [`purge_retain_multi`]: crate::core::bdd_manager::DDManager::purge_retain_multi
+    /// [`purge_retain`]: crate::core::bdd_manager::DDManager::purge_retain
+    pub fn create_slices_without_vars_exists(
+        &mut self,
+        roots: &[NodeID],
+        remove: &HashSet<VarID>,
+    ) -> HashMap<NodeID, NodeID> {
+        self.exists_multiple(roots, remove)
+    }
 }
 
 #[cfg(test)]
@@ -233,6 +328,64 @@ mod tests {
         assert_eq!(
             man.create_slice_without_vars_structural(root_bdd_without_a, &b),
             man.create_slice_without_vars_structural(root_bdd_without_b, &a),
+        );
+    }
+
+    #[test]
+    fn slice_exists_ab_ba_eq_1() {
+        let (man, root) =
+            DDManager::load_from_dddmp_file("examples/sandwich.dimacs.dddmp".to_string()).unwrap();
+        let root = root[0];
+
+        slice_exists_ab_ba_eq(man, root, VarID(5), VarID(15));
+    }
+
+    #[test]
+    fn slice_exists_ab_ba_eq_2() {
+        let (man, root) =
+            DDManager::load_from_dddmp_file("examples/sandwich.dimacs.dddmp".to_string()).unwrap();
+        let root = root[0];
+
+        slice_exists_ab_ba_eq(man, root, VarID(1), VarID(19));
+    }
+
+    fn slice_exists_ab_ba_eq(mut man: DDManager, root: NodeID, a: VarID, b: VarID) {
+        let a = [a].into_iter().collect::<HashSet<_>>();
+        let b = [b].into_iter().collect::<HashSet<_>>();
+
+        let root_bdd_without_a = man.create_slice_without_vars_exists(root, &a);
+        let root_bdd_without_b = man.create_slice_without_vars_exists(root, &b);
+
+        assert_eq!(
+            man.create_slice_without_vars_exists(root_bdd_without_a, &b),
+            man.create_slice_without_vars_exists(root_bdd_without_b, &a),
+        );
+    }
+
+    #[test]
+    fn slice_eq_exists_structural_1() {
+        let (man, root) =
+            DDManager::load_from_dddmp_file("examples/sandwich.dimacs.dddmp".to_string()).unwrap();
+        let root = root[0];
+
+        slice_eq_exists_structural(man, root, VarID(5), VarID(15));
+    }
+
+    #[test]
+    fn slice_eq_exists_structural_2() {
+        let (man, root) =
+            DDManager::load_from_dddmp_file("examples/sandwich.dimacs.dddmp".to_string()).unwrap();
+        let root = root[0];
+
+        slice_eq_exists_structural(man, root, VarID(1), VarID(19));
+    }
+
+    fn slice_eq_exists_structural(mut man: DDManager, root: NodeID, a: VarID, b: VarID) {
+        let vars = [a, b].into_iter().collect::<HashSet<_>>();
+
+        assert_eq!(
+            man.create_slice_without_vars_structural(root, &vars),
+            man.create_slice_without_vars_exists(root, &vars),
         );
     }
 }
